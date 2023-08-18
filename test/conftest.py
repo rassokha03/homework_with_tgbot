2ac6fc3984093ffef88c3abb4ccca15e5a192af4
Andrey Rassokhin <Andrey.Rassokhin@etalongroup.com>
2023-08-19 00:38:39 +0300

    update again

--- test/conftest.py
+++ test/conftest.py
@@ -1,21 +1,18 @@
 import os
 
 import pytest
-from selene.support.shared import browser
 from selenium import webdriver
-from dotenv import load_dotenv
 from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+from dotenv import load_dotenv
 
 from utils import attach
 
-DEFAULT_BROWSER_VERSION = "100.0"
+DEFAULT_BROWSER_VERSION = '100.0'
 
 
 def pytest_addoption(parser):
-    parser.addoption(
-        '--browser_version',
-        default='100.0'
-    )
+    parser.addoption('--browser_version', default='100.0')
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -23,33 +20,28 @@ def load_env():
     load_dotenv()
 
 
-@pytest.fixture(scope="function", autouse=True)
-def configure_browser(request):
-    browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
-    options = Options()
+@pytest.fixture(scope='function', autouse=False)
+def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version') or DEFAULT_BROWSER_VERSION
     selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": browser_version,
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
+        'browserName': 'chrome',
+        'browserVersion': browser_version,
+        'selenoid:options': {
+            'enableVNC': True,
+            'enableVideo': True
         }
     }
+    options = Options()
     options.capabilities.update(selenoid_capabilities)
 
     login = os.getenv('LOGIN')
     password = os.getenv('PASSWORD')
-
     driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        command_executor=f'https://{login}:{password}@selenoid.autotests.cloud/wd/hub',
         options=options
     )
 
-    browser.config.driver = driver
-    browser.config.base_url = "https://demoqa.com"
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
+    browser = Browser(Config(driver))
 
     yield browser
 
